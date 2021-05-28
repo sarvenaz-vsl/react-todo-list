06957e035ac1dc4f31bbdc366a5f716b01772895
sarvenaz-vsl <sarvenaz.vsl@gmail.com>
2021-05-28 23:21:35 +0430

    to do list (add, delete, edit) developed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import ListItems from './ListItems';
+import { library} from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(faTrash);
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+  
+  handleInput(e) {
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault(); //not refreshing page
+    const newItem = this.state.currentItem;
+
+    if(newItem.text !== '') {
+      const newItems = [newItem, ...this.state.items];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item => item.key !== key);
+    this.setState({
+      items:filteredItems
+    })
+  }
+
+  setUpdate(text, key){
+    const items = this.state.items;
+    items.map (item => {
+      if(item.key === key) {
+        item.text = text;
+      }
+      this.setState ({
+        items : items
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text.." value={this.state.currentItem.text} 
+            onChange={this.handleInput}></input>
+            <button type="submit">Add</button>
+          </form>
+        </header>
+        <ListItems items={this.state.items}  deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
+      </div>
+    )
+  }
 }
 
 export default App;
+
